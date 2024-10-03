e95d33a3ced47580ff2b1c3933b763363a190c80
alex <alexanderyi933@gmail.com>
2024-10-03 11:37:25 -0400

    refactored lambda handler for ease of testing and confirmed it still works when deployed on aws

--- src/lambdas/cloud_resume_handler.py
+++ src/lambdas/cloud_resume_handler.py
@@ -1,17 +1,44 @@
 import json, boto3
+from os import environ
 
 # need to specify region name in case it can't be inferred from something like
 # aws config files (e.g. when using cli client). In AWS console, I imagine region
 # name can also be inferred based on account info
 # here, though, there isn't anything to infer the region from, so specifying it explicitly
-client = boto3.client('dynamodb', region_name = "us-east-1")
+
+# per this github issue, might want to create the client inside the hanlder instead of outside?
+# https://github.com/getmoto/moto/issues/5493#issuecomment-1260138353
+# seems like it's possible that if client is created before any mocks from moto, the client would be reaching out to aws
+# instead of getting mocked?
+
+# also found this article and repo from aws for creating a lambda handler and associated aws resources
+# https://aws.amazon.com/blogs/devops/unit-testing-aws-lambda-with-python-and-mock-aws-services/
+# https://github.com/aws-samples/serverless-test-samples/blob/main/python-test-samples/lambda-mock/src/sample_lambda/app.py
+
+_LAMBDA_DYNAMO_DB_CLIENT = {
+    "client": boto3.client("dynamodb", region_name = environ.get("AWS_REGION", "us-east-1")),
+    "table_name": environ.get("DYNAMO_DB_TABLE_NAME", "NONE"),
+    "primary_key_value": environ.get("DYNAMO_DB_PK_VALUE", "NONE")
+}
+
+class DynamoDBClass:
+    def __init__(self, lambda_dynamo_db_client):
+        self.client = lambda_dynamo_db_client["client"]
+        self.table_name = lambda_dynamo_db_client["table_name"]
+        self.primary_key_value = lambda_dynamo_db_client["primary_key_value"]
 
 def lambda_handler(event, context):
+    global _LAMBDA_DYNAMO_DB_CLIENT
+
+    dynamodb_client_class = DynamoDBClass(_LAMBDA_DYNAMO_DB_CLIENT)
+
     try:
-        response = client.update_item(
-            TableName='WebsiteViewCount',
+        response = dynamodb_client_class.client.update_item(
+            TableName=dynamodb_client_class.table_name,
+            # wanted to set the 'site_name' in the 'Key' arg dynamically, but seems like I would have to loop to do that
+            # so I'm not going to bother and instead just hard code 'site_name' 
             Key = {
-                'site_name': {'S': 'cloud_resume_challenge'}
+                'site_name': {'S': dynamodb_client_class.primary_key_value}
             },
             # could also use 'SET view_count = view_count + :inc' as the update expression
             UpdateExpression = 'ADD view_count :inc',
@@ -27,7 +54,6 @@ def lambda_handler(event, context):
                 }
             }
         )
-        
         
         return {
             'statusCode': 200,
